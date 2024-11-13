4f8cda800125f8d8c5f51325d52aa018560734b6
unknown <nicholas.murphy2@mail.mcgill.ca>
2024-11-13 07:28:28 -0500

    multiple idft and ifft implemented

--- main.py
+++ main.py
@@ -27,12 +27,30 @@ def dft(x: NDArray[np.float64]) -> NDArray[Any]:
     return np.dot(e, x)
 
 
-def idft(x: NDArray[np.float64]):
-    pass
+def idft1(x: NDArray[np.float64]):
+    N = len(x)
+    out = []
+    for k in range(N):
+        xk = 0
+        for n in range(N):
+            e = np.exp(1j * 2 * math.pi * k * n * (1 / N))
+            xk += e * x[n]
+        out.append((1/N)*xk)
+    return out
 
+def idft(x: NDArray[np.float64]) -> NDArray[Any]:
+    N = len(x)
+    indices = np.arange(N)
+    kxn = indices * indices[:, None]
+    e = np.exp(1j * 2 * math.pi * kxn * (1 / N))
+    return np.dot((1/N)*e, x)
 
-def ifft(x: NDArray[np.float64]):
-    pass
+def ifft_helper(x: NDArray[np.float64]) -> NDArray[Any]:
+    N = len(x)
+    indices = np.arange(N)
+    kxn = indices * indices[:, None]
+    e = np.exp(1j * 2 * math.pi * kxn * (1 / N))
+    return np.dot(e, x)
 
 
 def fft(x: NDArray[np.float64]) -> NDArray[Any]:
@@ -56,6 +74,27 @@ def fft(x: NDArray[np.float64]) -> NDArray[Any]:
     return out
 
 
+def ifft(x: NDArray[np.float64]):
+    N = len(x)
+
+    if N <= 4:
+        return ifft_helper(x)
+
+    odd = ifft(x[1::2])
+    even = ifft(x[0::2])
+
+    out = []
+    for k in range(N // 2):
+        e = np.exp(1j * 2 * math.pi * k * (1 / N))
+        out.append((even[k] + e * odd[k]))
+    for k in range(N // 2):
+        e = np.exp(1j * 2 * math.pi * k * (1 / N))
+        out.append((even[k] - e * odd[k]))
+
+    out = (1/N)*np.array(out)
+    return out
+
+
 def fft2():
     pass
 
@@ -65,7 +104,7 @@ def ifft2():
 
 
 if __name__ == "__main__":
-    r = np.fft.fft(SAMPLE)
-    r2 = fft(SAMPLE)
+    r = np.fft.ifft(SAMPLE)
+    r2 = ifft(SAMPLE)
     print(np.allclose(r, r2))
     pass
