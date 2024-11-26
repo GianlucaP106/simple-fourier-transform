87acf02e4efc6e4079f3ab46b4d803ef67a63565
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2024-11-26 11:18:19 -0500

    readme and docstrings

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Simple Fourier transform
+
+## Installation
+
+```bash
+git clone https://github.com/GianlucaP106/simple-fourier-transform
+```
+
+## Run Modes
+
+```bash
+python fft.py -i ./assets/moonlanding.png -m 1|2|3|4
+```
+
+#### Modes
+
+1) Displays a log scale plot of the fourier transform of the provided image.
+2) Displays a denoised (using fourier transform) version of the provided image.
+3) Displays a the provided image compressed at various compression levels.
+4) Displays a runtime plot showing the difference between naive dft2 (2D) and fft2 (2d).
--- fft.py
+++ fft.py
@@ -11,6 +11,9 @@ from numpy._typing import NDArray
 
 
 def dft(x: NDArray) -> NDArray:
+    """
+    Naive implementation of dft.
+    """
     N = len(x)
     indices = np.arange(N)
     kxn = indices * indices[:, None]
@@ -19,6 +22,9 @@ def dft(x: NDArray) -> NDArray:
 
 
 def idft(x: NDArray) -> NDArray:
+    """
+    Naive implementation of idft.
+    """
     N = len(x)
     indices = np.arange(N)
     kxn = indices * indices[:, None]
@@ -27,6 +33,9 @@ def idft(x: NDArray) -> NDArray:
 
 
 def _idft(x: NDArray) -> NDArray:
+    """
+    Modified version of idft that doesnt scale by 1/N.
+    """
     N = len(x)
     indices = np.arange(N)
     kxn = indices * indices[:, None]
@@ -35,6 +44,9 @@ def _idft(x: NDArray) -> NDArray:
 
 
 def fft(x: NDArray) -> NDArray:
+    """
+    Implementation of fft (Cooley-Tukey).
+    """
     N = len(x)
 
     if N <= 4:
@@ -56,6 +68,10 @@ def fft(x: NDArray) -> NDArray:
 
 
 def ifft(x: NDArray):
+    """
+    Implementation of ifft (Cooley-Tukey).
+    """
+
     def rec(x: NDArray):
         N = len(x)
 
@@ -80,23 +96,45 @@ def ifft(x: NDArray):
 
 
 def dft2(x: NDArray):
+    """
+    Implementation of dft2 (2D) using naive dft.
+    """
     return np.transpose([dft(col) for col in np.transpose([dft(row) for row in x])])
 
 
+def idft2(x: NDArray):
+    """
+    Implementation of idft2 (2D) using naive dft.
+    """
+    return np.transpose([idft(col) for col in np.transpose([idft(row) for row in x])])
+
+
 def fft2(x: NDArray):
+    """
+    Implementation of fft2 (2D) using fft.
+    """
     return np.transpose([fft(col) for col in np.transpose([fft(row) for row in x])])
 
 
 def ifft2(x: NDArray):
+    """
+    Implementation of ifft2 (2D) using ifft.
+    """
     return np.transpose([ifft(col) for col in np.transpose([ifft(row) for row in x])])
 
 
 def load_gray_scale(path: str) -> MatLike:
+    """
+    Loads an image with grayscale.
+    """
     img = cv2.imread(path)
     return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
-def denoise(img: MatLike, cutoff=0.99) -> tuple[MatLike, int]:
+def denoise(img: MatLike, cutoff=0.985) -> tuple[MatLike, int]:
+    """
+    Denoises an image with sepecified cutoff
+    """
     # to fft
     freqs = fft2(img)
 
@@ -110,39 +148,29 @@ def denoise(img: MatLike, cutoff=0.99) -> tuple[MatLike, int]:
     # back to image
     return np.real(ifft2(freqs)), nonzero
 
-#testing different denoising
-def denoise2(img: MatLike, cutoff=0.99) -> tuple[MatLike, int]:
+
+def compression(img: MatLike, compression_level: float) -> tuple[MatLike, int]:
+    """
+    Compresses an image with sepecified compression level.
+    """
     # to fft
     freqs = fft2(img)
 
-    # zeroing
-    index_cutoff_low = int(cutoff * freqs.shape[0] * freqs.shape[1])
-    index_cutoff_high = int((1 - cutoff) * freqs.shape[0] * freqs.shape[1])
-    thresh_low, thresh_high = np.sort(np.abs(freqs.flatten()))[[index_cutoff_low, index_cutoff_high]]
-    freqs[(np.abs(freqs) < thresh_low) | (np.abs(freqs) > thresh_high)] = 0
-
-    nonzero = np.count_nonzero(freqs)
-
-    # back to image
-    return np.real(ifft2(freqs)), nonzero
-
-
-def compression(img: MatLike, compression_level: int) -> tuple[MatLike, int]:
-    #to fft
-    freqs = fft2(img)
-
-    #modify Fourier coefficients to compress 
+    # modify Fourier coefficients to compress
     index_cutoff = int(compression_level * freqs.shape[0] * freqs.shape[1])
     thresh = np.sort(np.abs(freqs.flatten()))[index_cutoff]
     freqs[np.abs(freqs) < thresh] = 0
 
     nonzero = np.count_nonzero(freqs)
 
-    #return the image to display
+    # return the image to display
     return np.real(ifft2(freqs)), nonzero
 
 
 def next_power_of_2(x):
+    """
+    Retuns the next power of 2
+    """
     if x == 0:
         return 1
     else:
@@ -150,6 +178,9 @@ def next_power_of_2(x):
 
 
 def load_image(path: str):
+    """
+    Loads and image with gray scale and resizes to the next power of 2.
+    """
     image = load_gray_scale(path)
     h, w = image.shape
     new_size = (next_power_of_2(w), next_power_of_2(h))
@@ -158,6 +189,9 @@ def load_image(path: str):
 
 
 def log_scale_plot(image, image_fft_form):
+    """
+    Displays a log scale plot of the fft.
+    """
     fig, ax = plt.subplots(1, 2, figsize=(10, 5))
     ax[0].imshow(image, cmap="gray")
     ax[0].set_title("Image")
@@ -167,6 +201,9 @@ def log_scale_plot(image, image_fft_form):
 
 
 def denoised_plot(image: MatLike, denoised_image: MatLike, count: int):
+    """
+    Displays the original image and a denoised version.
+    """
     fig, ax = plt.subplots(1, 2, figsize=(10, 5))
     ax[0].imshow(image, cmap="gray")
     ax[0].set_title("Image")
@@ -179,30 +216,21 @@ def denoised_plot(image: MatLike, denoised_image: MatLike, count: int):
     plt.show()
 
 
-def compress_plot(image: MatLike, compressed_image: MatLike, nonzero_count: int):
-    fig, ax = plt.subplots(1, 2, figsize=(10, 5))
-    ax[0].imshow(image, cmap="gray")
-    ax[0].set_title("Image")
-    ax[1].imshow(compressed_image, cmap="gray")
-    ax[1].set_title("Compressed Image")
-
-    print("number of nonzero values: ", nonzero_count)
-    total = image.shape[0] * image.shape[1]
-    print("nonzero / total: ", nonzero_count, "/", total, "=", nonzero_count / total)
-    plt.show()
-
 def compression_plot(image: MatLike):
+    """
+    Displayes the original image various compressions at different levels.
+    """
     fig, ax = plt.subplots(2, 3, figsize=(10, 5))
     compressed_image, nonzero_count = compression(image, 0)
     ax[0, 0].imshow(compressed_image, cmap="gray")
     ax[0, 0].set_title("Compression level 0%")
     print("0% nonzeros count: ", nonzero_count)
-    
+
     compressed_image1, nonzero_count1 = compression(image, 0.2)
     ax[0, 1].imshow(compressed_image1, cmap="gray")
     ax[0, 1].set_title("Compression level 20%")
     print("20% nonzeros count: ", nonzero_count1)
-    
+
     compressed_image2, nonzero_count2 = compression(image, 0.4)
     ax[0, 2].imshow(compressed_image2, cmap="gray")
     ax[0, 2].set_title("Compression level 40%")
@@ -216,8 +244,7 @@ def compression_plot(image: MatLike):
     compressed_image4, nonzero_count4 = compression(image, 0.8)
     ax[1, 1].imshow(compressed_image4, cmap="gray")
     ax[1, 1].set_title("Compression level 80%")
-    print("80% nonzeros count: ", nonzero_count4)\
-
+    print("80% nonzeros count: ", nonzero_count4)
     compressed_image5, nonzero_count5 = compression(image, 0.999)
     ax[1, 2].imshow(compressed_image5, cmap="gray")
     ax[1, 2].set_title("Compression level 99.9%")
@@ -227,8 +254,12 @@ def compression_plot(image: MatLike):
 
 
 def runtime_plot(sizes: NDArray):
+    """
+    Displayes a runtime plot of dft2 vs fft2
+    """
     powers_2 = 2**sizes
     futs = [dft2, fft2]
+    titles = ["DFT2", "FFT2"]
 
     _, ax = plt.subplots(1, 2, figsize=(10, 5))
     for fut_idx, fut in enumerate(futs):
@@ -245,9 +276,12 @@ def runtime_plot(sizes: NDArray):
             stds.append(np.std(times))
             average_times.append(np.average(times))
 
-        stds = 2 * np.array(stds)
+        stds = 3 * np.array(stds)
         ax[fut_idx].errorbar(powers_2, average_times, yerr=stds)
 
+        ax[fut_idx].set_title(titles[fut_idx])
+        ax[fut_idx].set_xlabel("Input size")
+        ax[fut_idx].set_ylabel("Runtime")
     plt.show()
 
 
@@ -265,8 +299,8 @@ def parse_args():
         "-i",
         "--image",
         type=str,
-        help="Image file path",
-        required=True,
+        help="Image file path (Default is ./assets/moonlanding.png)",
+        default="./assets/moonlanding.png",
     )
     return parser.parse_args()
 
@@ -280,17 +314,14 @@ def main():
         log_scale_plot(image, image_fft_form)
 
     elif args.mode == 2:
-        denoised_image, count = denoise2(image)
+        denoised_image, count = denoise(image)
         denoised_plot(image, denoised_image, count)
 
     elif args.mode == 3:
         compression_plot(image)
-        # compressed_image, nonzero_count = compression(image, 0)
-        # compress_plot(image, compressed_image, nonzero_count)
 
     elif args.mode == 4:
-        # TODO: fixe sizes according to what will take a a reasonable amount of time
-        runtime_plot(np.arange(11)[5:])
+        runtime_plot(np.arange(9)[5:])
 
 
 if __name__ == "__main__":
